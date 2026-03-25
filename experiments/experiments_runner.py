c8dc8ce888461d0d70b6ff2d8ac907930a67d92e
bkisly <b.kisly@gmail.com>
2026-03-25 21:39:03 +0100

    fix

--- experiments/experiments_runner.py
+++ experiments/experiments_runner.py
@@ -13,15 +13,6 @@ class ExperimentsRunner:
         self.evaluator = evaluator
         self.models = models
 
-    def test_single_metric(self, metric_key: MetricKeys, pieces: List[Piece], verbose: bool = False) -> Dict[str, float]:
-        metrics = self._test_average_metrics(pieces, verbose)
-        results_for_metric: Dict[str, float] = {}
-
-        for model_name in metrics:
-            results_for_metric[model_name] = metrics[model_name].to_dict()[metric_key]
-
-        return results_for_metric
-
     def test_tempo_robustness(self, pieces: List[Piece]) -> Dict[str, List[ExperimentVariation]]:
         pass
 
@@ -40,7 +31,7 @@ class ExperimentsRunner:
     def test_recovery_time(self, pieces: List[Piece]) -> Dict[str, Dict[int, float]]:
         pass
 
-    def _test_average_metrics(self, pieces: List[Piece], verbose: bool = False) -> Dict[str, EvaluationMetrics]:
+    def test_average_metrics(self, pieces: List[Piece], verbose: bool = False) -> Dict[str, EvaluationMetrics]:
         results = {}
 
         for model in self.models:
@@ -48,7 +39,7 @@ class ExperimentsRunner:
 
         iterator = tqdm(enumerate(pieces), total=len(pieces), disable=not verbose)
         for i, piece in iterator:
-            iterator.set_description(f"Evaluating piece no. {i}... "
+            iterator.set_description(f"Evaluating piece no. {i+1}... "
                                      f"(Path to MIDI: {piece.midi_path}, "
                                      f"path to audio: {piece.audio_path}, )")
 
